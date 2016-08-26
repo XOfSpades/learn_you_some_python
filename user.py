b5c18fa30270336375766dec26ac2174e49c9a55
bstoecker <bernhard.stoecker@invision.de>
2016-08-26 22:10:15 +0200

    For loop in user file

--- user.py
+++ user.py
@@ -1,5 +1,16 @@
-class User
-  def __init__(self, first_name, last_name, email)
+class User:
+  def __init__(self, first_name, last_name, email):
     self.first_name = first_name
     self.last_name = last_name
     self.email = email
+
+  def name(self):
+    print(self.first_name, self.last_name)
+
+users = [
+  User("Kira", "Katze", "Kira@Katze.com"),
+  User("Hugo", "Hase", "Hugo@Hase.com"),
+  User("Ingo", "Igel", "Ingo@Igel.com")
+]
+map user in users:
+  "Hallo " + user.name()
